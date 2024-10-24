c739f43898450ec18e525f88fb65e67cc7edf75d
Ptrckjs <patrickmjose22@gmail.com>
2024-10-25 00:19:32 +0800

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
@@ -15,11 +16,82 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({super.key});
 
   @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  String rollText = 'Low Roll';
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+
+      int totalDiceNumber = leftDiceNumber + rightDiceNumber;
+
+      if (totalDiceNumber <= 5) {
+        rollText = 'Low Roll';
+      } else {
+        rollText = 'High Roll';
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 80.0, bottom: 80.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      changeDiceFace();
+                    },
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () {
+                      changeDiceFace();
+                    },
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            rollText,
+            style: const TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            '5 and below = Low Roll\n6 and above = High Roll',
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
   }
 }
